7a805c83ee9b1205e7f1f038ad77c4514711b19f
Yan Lynn Aung <yan.lynn1991@gmail.com>
2023-10-03 11:12:48 +0800

    upload

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,28 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+// import { defineConfig } from 'vite';
+// import laravel from 'laravel-vite-plugin';
 
+// export default defineConfig({
+//     plugins: [
+//         laravel({
+//             input: ['resources/css/app.css', 'resources/js/app.js'],
+//             refresh: true,
+//         }),
+//     ],
+// });
+
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import laravel from 'laravel-vite-plugin';
+// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-        laravel({
+  plugins: [
+    
+    react(),
+    laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
-    ],
-});
+
+],
+})
